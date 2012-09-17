44ae225b946348be8dc197b102a6b6ff777879c6
Konstantin Tarkus <k.tarkus@gmail.com>
2012-09-17 04:19:45 +0400

    Initial commit

---
+++ src/App.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="KriaSoft LLC">
+//   Copyright © 2012 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("App.Core")]
+[assembly: AssemblyDescription("Web Application Core Library")]
---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CommonAssemblyInfo.cs" company="KriaSoft LLC">
+//   Copyright © 2012 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+// Common Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KriaSoft LLC")]
+[assembly: AssemblyProduct("Web Application Starter Kit")]
+[assembly: AssemblyCopyright("Copyright © 2012 KriaSoft LLC")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
