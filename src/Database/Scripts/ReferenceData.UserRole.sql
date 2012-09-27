9cf177a5661a4289de3b079602b569c023db4996
Konstantin Tarkus <k.tarkus@gmail.com>
2012-09-28 02:05:21 +0400

    Added post-deployment script into the Database project

---
+++ src/Database/Scripts/ReferenceData.UserRole.sql
@@ -0,0 +1,28 @@
+﻿--------------------------------------------------------------------------------------------------------------------
+-- Reference data for [Membership].[UserRole]
+--------------------------------------------------------------------------------------------------------------------
+
+--SET IDENTITY_INSERT [Membership].[UserRole] ON
+--GO
+
+MERGE INTO [Membership].[UserRole] AS Target
+USING (VALUES
+  (1, N'Administrator'),
+  (2, N'Moderator')
+)
+AS Source ([RoleID], [Name])
+ON Target.[RoleID] = Source.[RoleID]
+-- update matched rows
+WHEN MATCHED THEN
+UPDATE SET [Name] = Source.[Name]
+-- insert new rows
+WHEN NOT MATCHED BY TARGET THEN
+INSERT ([RoleID], [Name])
+VALUES ([RoleID], [Name]);
+-- delete rows that are in the target but not the source
+-- WHEN NOT MATCHED BY SOURCE THEN 
+-- DELETE
+GO
+
+--SET IDENTITY_INSERT [Membership].[UserRole] OFF
+--GO
