17103273b13f8216d12b5fc29ad32075d17658d6
Konstantin Tarkus <hello@tarkus.me>
2013-05-08 20:50:09 +0400

    Add EmailService with SendGrid integration

---
+++ Source/Core/Services/IEmailService.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IEmailService.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace App.Services
+{
+    using System.IO;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Reflection;
+
+    using RazorEngine;
+    using RazorEngine.Configuration;
+    using RazorEngine.Templating;
+
+    using SendGrid;
+    using SendGrid.Transport;
+
+    public interface IEmailService
+    {
+        void Send<T>(string to, string templateName, string subject, T data);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly NetworkCredential credentials;
+
+        public EmailService(NetworkCredential credentials)
+        {
+            Razor.SetTemplateService(new TemplateService(new TemplateServiceConfiguration
+            {
+                Resolver = new DelegateTemplateResolver(name =>
+                {
+                    using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("App.EmailTemplates.{0}.cshtml", name)))
+                    using (var reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                })
+            }));
+
+            this.credentials = credentials;
+        }
+
+        public void Send<T>(string to, string templateName, string subject, T data)
+        {
+            var mail = Mail.GetInstance();
+            var transportSMTP = SMTP.GetInstance(this.credentials);
+            var template = Razor.Resolve<T>(templateName, data);
+            var context = new ExecuteContext();
+            var message = template.Run(context);
+
+            mail.From = new MailAddress("noreply@example.com");
+            mail.Subject = subject;
+            mail.AddTo(to);
+            mail.Html = message;
+
+            transportSMTP.Deliver(mail);
+        }
+    }
+}
---
+++ Tests/Core.UnitTests/Services/EmailServiceTest.cs
@@ -0,0 +1,27 @@
+﻿namespace App.UnitTests.Services
+{
+    using System;
+    using System.Net;
+    using App.Services;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EmailServiceTest
+    {
+        [TestMethod]
+        public void Sends_an_Email()
+        {
+            // Arrange
+            var credentials = new NetworkCredential();
+            var service = new EmailService(credentials);
+            var data = new
+            {
+                SiteName = "Site Name",
+                EmailVerificationUrl = "http://www.sample.com/verify/1234567890"
+            };
+
+            // Act
+            service.Send("user@example.com", "WelcomeNewUser", "Welcome to Site", data);
+        }
+    }
+}
