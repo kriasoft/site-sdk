52d96da1e4e1993c81415ed8ec9ad732992f8f6d
Konstantin Tarkus <hello@tarkus.me>
2013-06-02 03:02:04 +0400

    Update 3rd party libraries and reference assemblies

--- Source/Web/Scripts/Site.js
+++ Source/Web/Scripts/Site.js
@@ -1,6 +1,4 @@
-﻿/// <reference path="jquery-1.8.3.js" />
-
-// Enable Google Analytics tracking if Google Analytics property ID is present in Web.config\appSettings\GoogleAnalyticsPID
+﻿// Enable Google Analytics tracking if Google Analytics property ID is present in Web.config\appSettings\GoogleAnalyticsPID
 if ($('body').data().ga) {
     var _gaq = [['_setAccount', $('body').data().ga], ['_trackPageview']];
 
